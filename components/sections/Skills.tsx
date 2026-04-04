bae451a4ed6057aaf83278917da60a38777c8de0
Aribdmn <aribdmn.dev@gmail.com>
2026-04-04 21:02:07 +0700

    updating list of skills

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -11,16 +11,17 @@ const Skills = () => {
 						</h2>
 						<p className="text-on-surface-variant">
 							The tools and languages I leverage to build robust, scalable
-							mobile applications across platforms.
+							applications across platforms.
 						</p>
 					</div>
 					<div className="flex gap-2 text-on-surface-variant font-label text-xs uppercase tracking-widest border-b border-outline-variant/20 pb-2">
 						<span>Expertise</span>
 						<span className="text-primary-dim">/</span>
-						<span>Full-Stack Mobile</span>
+						<span>Full-Stack</span>
 					</div>
 				</div>
-				<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
+				<div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
+					{/* front end */}
 					{/* <!-- Skill Card 1 --> */}
 					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
 						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
@@ -66,6 +67,58 @@ const Skills = () => {
 					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
 						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
 							<svg
+								role="img"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								className="text-primary-dim"
+							>
+								<path d="M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 C13.666,10.618,15.027,12,18.001,12c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 c1.177,1.194,2.538,2.576,5.512,2.576c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C10.337,13.382,8.976,12,6.001,12z" />
+							</svg>
+						</div>
+						<span className="font-headline font-bold text-on-surface">
+							Tailwind CSS
+						</span>
+					</div>
+					{/* <!-- Skill Card 4 --> */}
+					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
+						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
+							<svg
+								role="img"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+								className="text-primary-dim"
+								fill="currentColor"
+							>
+								<path d="M0 0h24v24H0V0zm22.034 18.276c-.175-1.095-.888-2.015-3.003-2.873-.736-.345-1.554-.585-1.797-1.14-.091-.33-.105-.51-.046-.705.15-.646.915-.84 1.515-.66.39.12.75.42.976.9 1.034-.676 1.034-.676 1.755-1.125-.27-.42-.404-.601-.586-.78-.63-.705-1.469-1.065-2.834-1.034l-.705.089c-.676.165-1.32.525-1.71 1.005-1.14 1.291-.811 3.541.569 4.471 1.365 1.02 3.361 1.244 3.616 2.205.24 1.17-.87 1.545-1.966 1.41-.811-.18-1.26-.586-1.755-1.336l-1.83 1.051c.21.48.45.689.81 1.109 1.74 1.756 6.09 1.666 6.871-1.004.029-.09.24-.705.074-1.65l.046.067zm-8.983-7.245h-2.248c0 1.938-.009 3.864-.009 5.805 0 1.232.063 2.363-.138 2.711-.33.689-1.18.601-1.566.48-.396-.196-.597-.466-.83-.855-.063-.105-.11-.196-.127-.196l-1.825 1.125c.305.63.75 1.172 1.324 1.517.855.51 2.004.675 3.207.405.783-.226 1.458-.691 1.811-1.411.51-.93.402-2.07.397-3.346.012-2.054 0-4.109 0-6.179l.004-.056z" />
+							</svg>
+						</div>
+						<span className="font-headline font-bold text-on-surface">
+							JavaScript
+						</span>
+					</div>
+					{/* <!-- Skill Card 5 --> */}
+					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
+						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
+							<svg
+								role="img"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+								className="text-primary-dim"
+								fill="currentColor"
+							>
+								<path d="M1.125 0C.502 0 0 .502 0 1.125v21.75C0 23.498.502 24 1.125 24h21.75c.623 0 1.125-.502 1.125-1.125V1.125C24 .502 23.498 0 22.875 0zm17.363 9.75c.612 0 1.154.037 1.627.111a6.38 6.38 0 0 1 1.306.34v2.458a3.95 3.95 0 0 0-.643-.361 5.093 5.093 0 0 0-.717-.26 5.453 5.453 0 0 0-1.426-.2c-.3 0-.573.028-.819.086a2.1 2.1 0 0 0-.623.242c-.17.104-.3.229-.393.374a.888.888 0 0 0-.14.49c0 .196.053.373.156.529.104.156.252.304.443.444s.423.276.696.41c.273.135.582.274.926.416.47.197.892.407 1.266.628.374.222.695.473.963.753.268.279.472.598.614.957.142.359.214.776.214 1.253 0 .657-.125 1.21-.373 1.656a3.033 3.033 0 0 1-1.012 1.085 4.38 4.38 0 0 1-1.487.596c-.566.12-1.163.18-1.79.18a9.916 9.916 0 0 1-1.84-.164 5.544 5.544 0 0 1-1.512-.493v-2.63a5.033 5.033 0 0 0 3.237 1.2c.333 0 .624-.03.872-.09.249-.06.456-.144.623-.25.166-.108.29-.234.373-.38a1.023 1.023 0 0 0-.074-1.089 2.12 2.12 0 0 0-.537-.5 5.597 5.597 0 0 0-.807-.444 27.72 27.72 0 0 0-1.007-.436c-.918-.383-1.602-.852-2.053-1.405-.45-.553-.676-1.222-.676-2.005 0-.614.123-1.141.369-1.582.246-.441.58-.804 1.004-1.089a4.494 4.494 0 0 1 1.47-.629 7.536 7.536 0 0 1 1.77-.201zm-15.113.188h9.563v2.166H9.506v9.646H6.789v-9.646H3.375z" />
+							</svg>
+						</div>
+						<span className="font-headline font-bold text-on-surface">
+							TypeScript
+						</span>
+					</div>
+
+					{/* <!-- Skill Card 5 --> */}
+					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
+						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
+							<svg
 								className="text-primary-dim"
 								aria-hidden="true"
 								xmlns="http://www.w3.org/2000/svg"
@@ -81,43 +134,6 @@ const Skills = () => {
 						</div>
 						<span className="font-headline font-bold text-on-surface">
 							Laravel
-						</span>
-					</div>
-					{/* <!-- Skill Card 4 --> */}
-					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
-						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
-							<svg
-								role="img"
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-								className="text-primary-dim"
-								fill="currentColor"
-							>
-								<path
-									stroke="currentColor"
-									d="M14.314 0L2.3 12 6 15.7 21.684.013h-7.357zm.014 11.072L7.857 17.53l6.47 6.47H21.7l-6.46-6.468 6.46-6.46h-7.37z"
-								/>
-							</svg>
-						</div>
-						<span className="font-headline font-bold text-on-surface">
-							Flutter
-						</span>
-					</div>
-					{/* <!-- Skill Card 5 --> */}
-					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
-						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
-							<svg
-								role="img"
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="currentColor"
-								className="text-primary-dim"
-							>
-								<path d="M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 C13.666,10.618,15.027,12,18.001,12c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624 c1.177,1.194,2.538,2.576,5.512,2.576c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624 C10.337,13.382,8.976,12,6.001,12z" />
-							</svg>
-						</div>
-						<span className="font-headline font-bold text-on-surface">
-							Tailwind CSS
 						</span>
 					</div>
 					{/* <!-- Skill Card 6 --> */}
@@ -160,19 +176,80 @@ const Skills = () => {
 							Rest API
 						</span>
 					</div>
+					{/* <!-- Skill Card 4 --> */}
+					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
+						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
+							<svg
+								role="img"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+								className="text-primary-dim"
+								fill="currentColor"
+							>
+								<path
+									stroke="currentColor"
+									d="M14.314 0L2.3 12 6 15.7 21.684.013h-7.357zm.014 11.072L7.857 17.53l6.47 6.47H21.7l-6.46-6.468 6.46-6.46h-7.37z"
+								/>
+							</svg>
+						</div>
+						<span className="font-headline font-bold text-on-surface">
+							Flutter
+						</span>
+					</div>
 					{/* <!-- Skill Card 8 --> */}
 					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
 						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								fill="currentColor"
-								className="bi bi-git"
 								viewBox="0 0 16 16"
+								className="text-primary-dim"
 							>
 								<path d="M15.698 7.287 8.712.302a1.03 1.03 0 0 0-1.457 0l-1.45 1.45 1.84 1.84a1.223 1.223 0 0 1 1.55 1.56l1.773 1.774a1.224 1.224 0 0 1 1.267 2.025 1.226 1.226 0 0 1-2.002-1.334L8.58 5.963v4.353a1.226 1.226 0 1 1-1.008-.036V5.887a1.226 1.226 0 0 1-.666-1.608L5.093 2.465l-4.79 4.79a1.03 1.03 0 0 0 0 1.457l6.986 6.986a1.03 1.03 0 0 0 1.457 0l6.953-6.953a1.03 1.03 0 0 0 0-1.457" />
 							</svg>
 						</div>
 						<span className="font-headline font-bold text-on-surface">Git</span>
+					</div>
+
+					{/* <!-- Skill Card 11 --> */}
+					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
+						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
+							<svg
+								role="img"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+								className="text-primary-dim"
+								fill="currentColor"
+							>
+								<path d="m12 1.608 12 20.784H0Z" />
+							</svg>
+						</div>
+						<span className="font-headline font-bold text-on-surface">
+							Vercel
+						</span>
+					</div>
+					{/* <!-- Skill Card 12 --> */}
+					<div className="group p-8 bg-surface-container rounded-2xl hover:bg-surface-container-high transition-all duration-300 flex flex-col items-center text-center">
+						<div className="w-12 h-12 mb-4 flex items-center justify-center text-primary-dim">
+							<svg
+								role="img"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+								className="text-primary-dim"
+								fill="currentColor"
+							>
+								<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+								<g
+									id="SVGRepo_tracerCarrier"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+								></g>
+								<g id="SVGRepo_iconCarrier">
+									<path d="M10.2 0v6.456L12 8.928l1.8-2.472V0zm3.6 6.456v3.072l2.904-.96L20.52 3.36l-2.928-2.136zm2.904 2.112l-1.8 2.496 2.928.936 6.144-1.992-1.128-3.432zM17.832 12l-2.928.936 1.8 2.496 6.144 1.992 1.128-3.432zm-1.128 3.432l-2.904-.96v3.072l3.792 5.232 2.928-2.136zM13.8 17.544L12 15.072l-1.8 2.472V24h3.6zm-3.6 0v-3.072l-2.904.96L3.48 20.64l2.928 2.136zm-2.904-2.112l1.8-2.496L6.168 12 .024 13.992l1.128 3.432zM6.168 12l2.928-.936-1.8-2.496-6.144-1.992-1.128 3.432zm1.128-3.432l2.904.96V6.456L6.408 1.224 3.48 3.36Z"></path>
+								</g>
+							</svg>
+						</div>
+						<span className="font-headline font-bold text-on-surface">JWT</span>
 					</div>
 				</div>
 			</div>
